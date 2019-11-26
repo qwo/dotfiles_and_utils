ad98106773df56e4e9a78c83684a4007fbfa051e
Stanley Zheng <stanley@stanzheng.com>
2019-11-26 11:24:52 -0500

    Update setup_osx.sh

--- osx/setup_osx.sh
+++ osx/setup_osx.sh
@@ -35,6 +35,7 @@ cp .bashrc ~/.bashrc
 
 # install some bash tools
 git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+~/.bash_it/install.sh
 
 # install dev tools
 brew install node
