ddfbec9219580ef8ea15a3d97e0465fdb3c6c7ec
Stanley Zheng <stanley@zheng.nyc>
2020-05-25 12:51:26 -0400

    Update ubuntu20.sh

--- lenovo-x1-black7/ubuntu20.sh
+++ lenovo-x1-black7/ubuntu20.sh
@@ -9,3 +9,8 @@ sudo apt-get install python3.6
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt-get install ./google-chrome-stable_current_amd64.deb 
 
+
+# 
+snap install code-insiders --classic
+
+
