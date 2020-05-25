ff44130c023c0953b4c61c492cecd46cff0695a4
Stanley Zheng <stanley@zheng.nyc>
2020-05-25 12:49:00 -0400

    Update and rename ubuntu18.sh to ubuntu20.sh

--- lenovo-x1-black7/ubuntu18.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-sudo apt-get install -y software-properties-common
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt-get update
-sudo apt-get install python3.6
---
+++ lenovo-x1-black7/ubuntu20.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
+sudo apt-get install python3.6
+
+# 
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt-get install ./google-chrome-stable_current_amd64.deb 
+
