d3c43f91800818414865c0d1325b85cf6557495b
Stanley Zheng <stanley@zheng.nyc>
2020-05-25 14:25:54 -0400

    updating package install

--- lenovo-x1-black7/ubuntu20.sh
+++ lenovo-x1-black7/ubuntu20.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/bash
 
+sudo apt-get install -y git curl
 sudo apt-get install -y software-properties-common
-sudo add-apt-repository ppa:deadsnakes/ppa
+sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt-get update
 sudo apt-get install python3.6
+sudo
 
 # 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
