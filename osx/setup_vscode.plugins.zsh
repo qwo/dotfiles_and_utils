5ea0b08342357b6e45f95bbf8b1c3a620270aa3a
Stanley Zheng <stanley@zheng.nyc>
2022-10-29 23:22:04 -0400

    add it all

--- osx/setup_vscode.plugins.zsh
+++ osx/setup_vscode.plugins.zsh
@@ -22,8 +22,48 @@ code --enable-proposed-api (<extension-id>)
 # provide '@<version>' to install a specific version, for example: 'christian-kohler.path-intellisense@1.2.3'.
 """
 
+
+
 arraylist=(
     christian-kohler.path-intellisense
+    bradlc.vscode-tailwindcss
+    bungcip.better-toml
+    GitHub.codespaces
+    GitHub.copilot
+    GitHub.copilot-nightly
+    hashicorp.terraform
+    LeetCode.vscode-leetcode
+    mechatroner.rainbow-csv
+    mgesbert.python-path
+    mikestead.dotenv
+    ms-azuretools.vscode-docker
+    ms-python.python
+    ms-python.vscode-pylance
+    ms-toolsai.jupyter
+    ms-toolsai.jupyter-keymap
+    ms-toolsai.jupyter-renderers
+    ms-toolsai.vscode-jupyter-cell-tags
+    ms-toolsai.vscode-jupyter-slideshow
+    ms-vscode-remote.remote-containers
+    ms-vscode-remote.remote-ssh
+    ms-vscode-remote.remote-ssh-edit
+    ms-vscode-remote.remote-wsl
+    ms-vscode.cpptools
+    ms-vscode.powershell
+    ms-vsliveshare.vsliveshare
+    platformio.platformio-ide
+    redhat.java
+    redhat.vscode-xml
+    ritwickdey.LiveServer
+    VisualStudioExptTeam.intellicode-api-usage-examples
+    VisualStudioExptTeam.vscodeintellicode
+    vscjava.vscode-java-debug
+    vscjava.vscode-java-dependency
+    vscjava.vscode-java-pack
+    vscjava.vscode-java-test
+    vscjava.vscode-maven
+    waderyan.gitblame
+    yzhang.markdown-all-in-one
 )
 for i in "${arraylist[@]}"; 
     do code --install-extension $i
