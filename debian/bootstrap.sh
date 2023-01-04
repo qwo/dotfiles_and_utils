00067a03479969543eaffdb12992a2a91874ac04
Stanley Zheng <stanley@zheng.nyc>
2023-01-03 19:23:48 -0500

    updating bootstrapping for lib secret save

--- debian/bootstrap.sh
+++ debian/bootstrap.sh
@@ -5,6 +5,10 @@ sudo apt-get update -y
 # install system libraries
 sudo apt-get install curl wget git -y 
 
+#secret saving
+
+sudo apt-get install libsecret-1-0 libsecret-1-dev
+
 
 # install python libraries 
 curl -sSL https://install.python-poetry.org > install_poetry && python3 install_poetry --version 1.2.0b2
@@ -14,3 +18,4 @@ curl -sSL https://install.python-poetry.org > install_poetry && python3 install_
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 nvm install 16
 nvm use 16
+
